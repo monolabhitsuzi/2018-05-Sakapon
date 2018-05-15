e652a19c98330e5247daffa516c1b6f887c108ad
Keiho Sakapon <git@saka-pon.net>
2018-05-15 12:10:06 +0900

    Use ValueTuple

--- SpiralNumbers/SpiralNumbersConsole2/Program.cs
+++ SpiralNumbers/SpiralNumbersConsole2/Program.cs
@@ -29,20 +29,20 @@ namespace SpiralNumbersConsole2
         {
             var matrix = new int[Width, Height];
             if (matrix.Length == 0) return matrix;
-            var delta = new Int32Vector { X = 1 };
+            var delta = (X: 1, Y: 0);
 
-            bool IsValid(Int32Vector _p) => IsInRange(_p) && matrix[_p.X, _p.Y] == 0;
+            bool IsValid((int X, int Y) _p) => IsInRange(_p) && matrix[_p.X, _p.Y] == 0;
 
-            Int32Vector NextPoint(Int32Vector _p)
+            (int X, int Y) NextPoint((int X, int Y) _p)
             {
-                var temp = _p + delta;
+                var temp = Add(_p, delta);
                 if (IsValid(temp)) return temp;
 
                 delta = TurnRight(delta);
-                return _p + delta;
+                return Add(_p, delta);
             }
 
-            var p = new Int32Vector();
+            var p = (X: 0, Y: 0);
             matrix[p.X, p.Y] = 1;
 
             for (var i = 2; i <= matrix.Length; i++)
@@ -53,18 +53,10 @@ namespace SpiralNumbersConsole2
             return matrix;
         }
 
-        static bool IsInRange(Int32Vector p) => 0 <= p.X && p.X < Width && 0 <= p.Y && p.Y < Height;
-        static Int32Vector TurnRight(Int32Vector v) => new Int32Vector { X = -v.Y, Y = v.X };
+        static bool IsInRange((int X, int Y) p) => 0 <= p.X && p.X < Width && 0 <= p.Y && p.Y < Height;
+        static (int X, int Y) Add((int X, int Y) v1, (int X, int Y) v2) => (X: v1.X + v2.X, Y: v1.Y + v2.Y);
+        static (int X, int Y) TurnRight((int X, int Y) v) => (X: -v.Y, Y: v.X);
 
         static string JoinStrings(this IEnumerable<string> source, string separator) => string.Join(separator, source);
-    }
-
-    public struct Int32Vector
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public static Int32Vector operator +(Int32Vector v1, Int32Vector v2) => new Int32Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
-        public override string ToString() => $"({X}, {Y})";
     }
 }
